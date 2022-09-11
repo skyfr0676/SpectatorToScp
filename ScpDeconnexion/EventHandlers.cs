2c05db186bc7dac92a46f2ac043a6782905a04db
skyyt15 <doublecraft76ytb@gmail.com>
2022-09-11 16:20:40 +0200

    Create plugin

---
+++ ScpDeconnexion/EventHandlers.cs
@@ -0,0 +1,26 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System;
+using System.Collections.Generic;
+
+namespace ScpDeconnexion
+{
+    public class EventHandlers
+    {
+        public List<Player> SpectatorPlayer = new List<Player>();
+        public void Disconnect(LeftEventArgs ev)
+        {
+            if (ev.Player.Role.Side == Exiled.API.Enums.Side.Scp)
+            {
+                Log.Debug("test");
+            }
+            foreach (Player plr in Player.List)
+            {
+                SpectatorPlayer.Add(plr);
+            }
+            Random random = new Random();
+            int test = random.Next(SpectatorPlayer.Count);
+            Log.Warn(test);
+        }
+    }
+}
---
+++ ScpDeconnexion/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Event = Exiled.Events.Handlers;
+
+namespace ScpDeconnexion
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "sky";
+
+        public override string Name { get; } = "ScpDeconnexion";
+
+        public override string Prefix { get; } = "AntiScpD";
+        
+        public override Version Version { get; } = new Version(1, 0, 0);
+
+        public override Version RequiredExiledVersion { get; } = new Version(5, 3, 0);
+        
+        public static Plugin Singleton;
+
+        public static EventHandlers Handlers;
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            Log.Debug("Hello ! i'm actually Registering All Events and List...", Config.Debug);
+            Log.Debug("Loaded !",Config.Debug);
+            Handlers = new EventHandlers();
+            Event.Player.Left += Handlers.Disconnect;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Log.Info("goodbye !");
+            Event.Player.Left -= Handlers.Disconnect;
+            Handlers = null;
+            Singleton = null;
+            base.OnDisabled();
+        }
+    }
+}
